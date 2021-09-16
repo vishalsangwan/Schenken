c61fd5893143688cc78c9a7146473f6c27ff58bc
Vishal Sangwan <vishalsangwan10@gmail.com>
2021-09-16 23:48:05 +0200

    image loading from firebase

--- lib/main.dart
+++ lib/main.dart
@@ -6,70 +6,50 @@ import 'package:firebase_core/firebase_core.dart';
 
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
-import 'package:vershenken/display_widgets.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   firebase_storage.ListResult result =
       await firebase_storage.FirebaseStorage.instance.ref().listAll();
-
-  result.items.forEach((firebase_storage.Reference ref) {
-    print('Found file: $ref');
-  });
-
-  result.prefixes.forEach((firebase_storage.Reference ref) {
-    print('Found directory: $ref');
-  });
-  runApp(MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text("Items"),
-          ),
-          body: App())));
-}
-
-/// We are using a StatefulWidget such that we only create the [Future] once,
-/// no matter how many times our widget rebuild.
-/// If we used a [StatelessWidget], in the event where [App] is rebuilt, that
-/// would re-initialize FlutterFire and make our application re-enter loading state,
-/// which is undesired.
-class App extends StatefulWidget {
-  const App({Key? key}) : super(key: key);
-
-  // Create the initialization Future outside of `build`:
-  @override
-  _AppState createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  /// The future is part of the state of our widget. We should not call `initializeApp`
-  /// directly inside [build].
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-  
-  
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      // Initialize FlutterFire:
-      future: _initialization,
-      builder: (context, snapshot) {
-        // Check for errors
-        if (snapshot.hasError) {
-          return const Text("Vishal has error");
-        }
-
-        // Once complete, show your application
-        if (snapshot.connectionState == ConnectionState.done) {
-      
-          return gallery;
-        }
-
-        // Otherwise, show something whilst waiting for initialization to complete
-        return const Text("vishal");
-
-      },
+  List<Widget> allImageBox = [];
+  for (var ref in result.items) {
+    var URL = await ref.getDownloadURL();
+    
+    allImageBox.add(
+      Container(
+        padding: const EdgeInsets.all(8),
+        child:  Image.network(URL),
+        color: Colors.green[300],
+      ),
     );
   }
+
+
+
+  runApp(MaterialApp(
+      home: Scaffold(
+    appBar: AppBar(
+      title: const Text("All Items"),
+    ),
+    body: CustomScrollView(
+      primary: false,
+      slivers: <Widget>[
+        SliverPadding(
+          padding: const EdgeInsets.all(10),
+          sliver: SliverGrid.count(
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            crossAxisCount: 2,
+            children: allImageBox,
+          ),
+        ),
+      ],
+    )
+
+    )
+  )
+  );
 }
 
